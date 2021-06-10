07e3c484a954bafb9c71d95eb1ec971b7251f9bb
Guillaume Malek <guivome@gmail.com>
2021-06-10 11:28:26 +0200

    update environment variable

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const devEnv = {
   production: false,
   dbUri: 'mongodb+srv://root:root@cluster0.bymnq.mongodb.net/Timey',
-  apiUrl: '/timey',
+  apiUrl: '',
   port: '8001'
 }
--- src/models/environment.model.ts
+++ src/models/environment.model.ts
@@ -1,7 +1,7 @@
 export class Environment {
   production = true
   dbUri = 'mongodb+srv://root:root@cluster0.bymnq.mongodb.net/Timey'
-  apiUrl = '/timey'
+  apiUrl = ''
   port = '3000'
 
   constructor(data: Partial<Environment>) {
