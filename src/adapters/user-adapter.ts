985b812d7fe38be572de7476ee4dd91d555b8235
Guillaume Malek <guillaume.malek@telecomnancy.eu>
2021-06-15 16:44:51 +0200

    fix model send email

--- src/adapters/user-adapter.ts
+++ src/adapters/user-adapter.ts
@@ -2,7 +2,7 @@ import {IUser, User} from '../models/users/user.model'
 
 export const UserAdapter = {
   adapt(user: IUser) {
-    const userMini = new User({_id: user._id, tasks: user.tasks, tags: user.tags, parameters: user.parameters})
+    const userMini = new User({_id: user._id, email: user.email, tasks: user.tasks, tags: user.tags, parameters: user.parameters})
     return userMini
   }
 }
